0605d9782ded11087fef7669ba560c5b77fdef15
Ryan Gartin <ryan@eagledevelopers.net>
2016-11-20 22:56:12 -0500

    update for swift 3; force imageUrl in parity with videoUrl; see next commit for why

--- PHAsset+identifier.swift
+++ PHAsset+identifier.swift
@@ -1,6 +1,6 @@
 //
 //  PHAsset+identifier.swift
-//  
+//
 //
 //  kudos tos666
 //  https://forums.developer.apple.com/people/tos666
@@ -13,43 +13,41 @@ public extension PHAsset
 {
     public class func ah_fetchAssetWithLocalIdentifier(identifier: String, options: PHFetchOptions?) -> PHAsset?
     {
-        if let asset = PHAsset.fetchAssetsWithLocalIdentifiers([identifier], options: options).lastObject as? PHAsset
+        if let asset = PHAsset.fetchAssets(withLocalIdentifiers: [identifier], options: options).lastObject
         {
             return asset
         }
         
         var result : PHAsset?
         
-        let userAlbums = PHAssetCollection.fetchAssetCollectionsWithType(.Album, subtype: .AlbumMyPhotoStream, options: options)
+        let userAlbums = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .albumMyPhotoStream, options: options)
         
         let fetchOptions = PHFetchOptions()
         fetchOptions.predicate = NSPredicate(format:"localIdentifier ==[cd] %@", identifier)
         
-        userAlbums.enumerateObjectsUsingBlock
+        userAlbums.enumerateObjects({
+            (objectCollection : AnyObject, _ : Int, stopCollectionEnumeration : UnsafeMutablePointer<ObjCBool>) in
+            
+            guard let collection = objectCollection as? PHAssetCollection else
             {
-                (objectCollection : AnyObject, _ : Int, stopCollectionEnumeration : UnsafeMutablePointer<ObjCBool>) in
+                return
+            }
+            
+            let assetsFetchResult = PHAsset.fetchAssets(in: collection, options:fetchOptions)
+            
+            assetsFetchResult.enumerateObjects({
+                (objectAsset : AnyObject, _ : Int, stopAssetEnumeration: UnsafeMutablePointer<ObjCBool>) in
                 
-                guard let collection = objectCollection as? PHAssetCollection else
+                guard let asset = objectAsset as? PHAsset else
                 {
                     return
                 }
                 
-                let assetsFetchResult = PHAsset.fetchAssetsInAssetCollection(collection, options:fetchOptions)
-                
-                assetsFetchResult.enumerateObjectsUsingBlock
-                    {
-                        (objectAsset : AnyObject, _ : Int, stopAssetEnumeration: UnsafeMutablePointer<ObjCBool>) in
-                        
-                        guard let asset = objectAsset as? PHAsset else
-                        {
-                            return
-                        }
-                        
-                        result = asset
-                        stopAssetEnumeration.initialize(true)
-                        stopCollectionEnumeration.initialize(true)
-                }
-        }
+                result = asset
+                stopAssetEnumeration.initialize(to: true)
+                stopCollectionEnumeration.initialize(to: true)
+            })
+        })
         
         return result
     }
--- PHPhotoLibrary+PhotoAsset.swift
+++ PHPhotoLibrary+PhotoAsset.swift
@@ -1,9 +1,8 @@
 //
 //  PHPhotoLibrary+PhotoAsset.swift
-//  
 //
-//  kudos to ricardopereira
-//  https://gist.github.com/ricardopereira
+//
+//  https://github.com/kv2/PHPhotoLibrary-PhotoAsset.swift
 //
 
 import Foundation
@@ -15,14 +14,14 @@ public extension PHPhotoLibrary {
     typealias PhotoAsset = PHAsset
     typealias PhotoAlbum = PHAssetCollection
     
-    static func saveImage(image: UIImage, albumName: String, completion: (PHAsset?)->()) {
-        if let album = self.findAlbum(albumName) {
-            saveImage(image, album: album, completion: completion)
+    static func saveImage(imageUrl: URL, albumName: String, completion: @escaping (PHAsset?)->()) {
+        if let album = self.findAlbum(albumName: albumName) {
+            saveImage(imageUrl: imageUrl, album: album, completion: completion)
             return
         }
-        createAlbum(albumName) { album in
+        createAlbum(albumName: albumName) { album in
             if let album = album {
-                self.saveImage(image, album: album, completion: completion)
+                self.saveImage(imageUrl: imageUrl, album: album, completion: completion)
             }
             else {
                 assert(false, "Album is nil")
@@ -30,14 +29,14 @@ public extension PHPhotoLibrary {
         }
     }
     
-    static func saveVideo(videoUrl: NSURL, albumName: String, completion: (PHAsset?)->()) {
-        if let album = self.findAlbum(albumName) {
-            saveVideo(videoUrl, album: album, completion: completion)
+    static func saveVideo(videoUrl: URL, albumName: String, completion: @escaping (PHAsset?)->()) {
+        if let album = self.findAlbum(albumName: albumName) {
+            saveVideo(videoUrl: videoUrl, album: album, completion: completion)
             return
         }
-        createAlbum(albumName) { album in
+        createAlbum(albumName: albumName) { album in
             if let album = album {
-                self.saveVideo(videoUrl, album: album, completion: completion)
+                self.saveVideo(videoUrl: videoUrl, album: album, completion: completion)
             }
             else {
                 assert(false, "Album is nil")
@@ -45,13 +44,16 @@ public extension PHPhotoLibrary {
         }
     }
     
-    static private func saveImage(image: UIImage, album: PhotoAlbum, completion: (PHAsset?)->()) {
+    static private func saveImage(imageUrl: URL, album: PhotoAlbum, completion: @escaping (PHAsset?)->()) {
         var placeholder: PHObjectPlaceholder?
-        PHPhotoLibrary.sharedPhotoLibrary().performChanges({
+        PHPhotoLibrary.shared().performChanges({
             // Request creating an asset from the image
-            let createAssetRequest = PHAssetChangeRequest.creationRequestForAssetFromImage(image)
+            guard let createAssetRequest = PHAssetChangeRequest.creationRequestForAssetFromImage(atFileURL: imageUrl) else {
+                assert(false, "Error with image url")
+                return
+            }
             // Request editing the album
-            guard let albumChangeRequest = PHAssetCollectionChangeRequest(forAssetCollection: album) else {
+            guard let albumChangeRequest = PHAssetCollectionChangeRequest(for: album) else {
                 assert(false, "Album change request failed")
                 return
             }
@@ -61,34 +63,35 @@ public extension PHPhotoLibrary {
                 return
             }
             placeholder = photoPlaceholder
-            albumChangeRequest.addAssets([photoPlaceholder])
-            }, completionHandler: { success, error in
-                guard let placeholder = placeholder else {
-                    assert(false, "Placeholder is nil")
-                    completion(nil)
-                    return
-                }
-                
-                if success {
-                    completion(PHAsset.ah_fetchAssetWithLocalIdentifier(placeholder.localIdentifier, options:nil))
-                }
-                else {
-                    print(error)
-                    completion(nil)
-                }
+            let fastEnumeration = NSArray(array: [placeholder!] as [PHObjectPlaceholder])
+            albumChangeRequest.addAssets(fastEnumeration)
+        }, completionHandler: { success, error in
+            guard let placeholder = placeholder else {
+                assert(false, "Placeholder is nil")
+                completion(nil)
+                return
+            }
+            
+            if success {
+                completion(PHAsset.ah_fetchAssetWithLocalIdentifier(identifier: placeholder.localIdentifier, options:nil))
+            }
+            else {
+                print(error)
+                completion(nil)
+            }
         })
     }
-     
-    static private func saveVideo(videoUrl: NSURL, album: PhotoAlbum, completion: (PHAsset?)->()) {
     
+    static private func saveVideo(videoUrl: URL, album: PhotoAlbum, completion: @escaping (PHAsset?)->()) {
+        
         var placeholder: PHObjectPlaceholder?
         
-        PHPhotoLibrary.sharedPhotoLibrary().performChanges({
+        PHPhotoLibrary.shared().performChanges({
             
             // Request creating an asset from the image
-            let createAssetRequest = PHAssetChangeRequest.creationRequestForAssetFromVideoAtFileURL(videoUrl)
+            let createAssetRequest = PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: videoUrl)
             // Request editing the album
-            guard let albumChangeRequest = PHAssetCollectionChangeRequest(forAssetCollection: album) else {
+            guard let albumChangeRequest = PHAssetCollectionChangeRequest(for: album) else {
                 assert(false, "Album change request failed")
                 return
             }
@@ -100,82 +103,81 @@ public extension PHPhotoLibrary {
             }
             
             placeholder = videoPlaceholder
-        
-            albumChangeRequest.addAssets([videoPlaceholder])
-            }, completionHandler: { success, error in
-                guard let placeholder = placeholder else {
-                    assert(false, "Placeholder is nil")
-                    completion(nil)
-                    return
-                }
-                
-                if success {
-                    completion(PHAsset.ah_fetchAssetWithLocalIdentifier(placeholder.localIdentifier, options:nil))
-                }
-                else {
-                    print(error)
-                    completion(nil)
-                }
+            let fastEnumeration = NSArray(array: [videoPlaceholder] as [PHObjectPlaceholder])
+            albumChangeRequest.addAssets(fastEnumeration)
+        }, completionHandler: { success, error in
+            guard let placeholder = placeholder else {
+                assert(false, "Placeholder is nil")
+                completion(nil)
+                return
+            }
+            
+            if success {
+                completion(PHAsset.ah_fetchAssetWithLocalIdentifier(identifier: placeholder.localIdentifier, options:nil))
+            }
+            else {
+                print(error)
+                completion(nil)
+            }
         })
-    
+        
         
     }
-
-
+    
+    
     static func findAlbum(albumName: String) -> PhotoAlbum? {
         let fetchOptions = PHFetchOptions()
         fetchOptions.predicate = NSPredicate(format: "title = %@", albumName)
-        let fetchResult = PHAssetCollection.fetchAssetCollectionsWithType(.Album, subtype: .AlbumRegular, options: fetchOptions)
-        guard let photoAlbum = fetchResult.firstObject as? PHAssetCollection else {
+        let fetchResult = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .albumRegular, options: fetchOptions)
+        guard let photoAlbum = fetchResult.firstObject else {
             return nil
         }
         return photoAlbum
     }
     
-    static func createAlbum(albumName: String, completion: (PhotoAlbum?)->()) {
+    static func createAlbum(albumName: String, completion: @escaping (PhotoAlbum?)->()) {
         var albumPlaceholder: PHObjectPlaceholder?
-        PHPhotoLibrary.sharedPhotoLibrary().performChanges({
+        PHPhotoLibrary.shared().performChanges({
             // Request creating an album with parameter name
-            let createAlbumRequest = PHAssetCollectionChangeRequest.creationRequestForAssetCollectionWithTitle(albumName)
+            let createAlbumRequest = PHAssetCollectionChangeRequest.creationRequestForAssetCollection(withTitle: albumName)
             // Get a placeholder for the new album
             albumPlaceholder = createAlbumRequest.placeholderForCreatedAssetCollection
-            }, completionHandler: { success, error in
-                guard let placeholder = albumPlaceholder else {
-                    assert(false, "Album placeholder is nil")
-                    completion(nil)
-                    return
-                }
-                
-                let fetchResult = PHAssetCollection.fetchAssetCollectionsWithLocalIdentifiers([placeholder.localIdentifier], options: nil)
-                guard let album = fetchResult.firstObject as? PhotoAlbum else {
-                    assert(false, "FetchResult has no PHAssetCollection")
-                    completion(nil)
-                    return
-                }
-                
-                if success {
-                    completion(album)
-                }
-                else {
-                    print(error)
-                    completion(nil)
-                }
+        }, completionHandler: { success, error in
+            guard let placeholder = albumPlaceholder else {
+                assert(false, "Album placeholder is nil")
+                completion(nil)
+                return
+            }
+            
+            let fetchResult = PHAssetCollection.fetchAssetCollections(withLocalIdentifiers: [placeholder.localIdentifier], options: nil)
+            guard let album = fetchResult.firstObject else {
+                assert(false, "FetchResult has no PHAssetCollection")
+                completion(nil)
+                return
+            }
+            
+            if success {
+                completion(album)
+            }
+            else {
+                print(error)
+                completion(nil)
+            }
         })
     }
     
-    static func loadThumbnailFromLocalIdentifier(localIdentifier: String, completion: (UIImage?)->()) {
-        guard let asset = PHAsset.ah_fetchAssetWithLocalIdentifier(localIdentifier, options:nil) else {
+    static func loadThumbnailFromLocalIdentifier(localIdentifier: String, completion: @escaping (UIImage?)->()) {
+        guard let asset = PHAsset.ah_fetchAssetWithLocalIdentifier(identifier: localIdentifier, options:nil) else {
             completion(nil)
             return
         }
-        loadThumbnailFromAsset(asset, completion: completion)
+        loadThumbnailFromAsset(asset: asset, completion: completion)
     }
     
-    static func loadThumbnailFromAsset(asset: PhotoAsset, completion: (UIImage?)->()) {
-        PHImageManager.defaultManager().requestImageForAsset(asset, targetSize: CGSize(width: 100.0, height: 100.0), contentMode: .AspectFit, options: PHImageRequestOptions(), resultHandler: { result, info in
+    static func loadThumbnailFromAsset(asset: PhotoAsset, completion: @escaping (UIImage?)->()) {
+        PHImageManager.default().requestImage(for: asset, targetSize: CGSize(width: 100.0, height: 100.0), contentMode: .aspectFit, options: PHImageRequestOptions(), resultHandler: { result, info in
             completion(result)
         })
     }
     
 }
-
